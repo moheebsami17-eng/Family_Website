e394ce1bd824103d91fd2ad1325a83e835c86624
moheeb sami <moheebsami17@gmail.com>
2026-08-01 15:42:50 +0100

    444333

---
+++ contacts.js
@@ -0,0 +1,14 @@
+// كود ملف contacts.js الجديد
+const familyContacts = [
+    { id: 1, name: "سامي حميدان أبو لحية", relation: "الوالد / عميد العائلة", phone: "970598204148", avatar: "images/father.png", status: "online" },
+    { id: 2, name: "الوالدة الغالية (أم وائل)", relation: "الوالدة / بركة العائلة", phone: "970598204148", avatar: "images/mother.png", status: "online" },
+    { id: 3, name: "محمد (وائل)", relation: "الفرع 1", phone: "970598204148", avatar: "images/wael.png", status: "online" },
+    { id: 4, name: "جودت", relation: "الفرع 2", phone: "970598204148", avatar: "images/jawdat.png", status: "busy" },
+    { id: 5, name: "جيهان", relation: "الفرع 3", phone: "970598204148", avatar: "images/jihan.png", status: "offline" },
+    { id: 6, name: "أسماء", relation: "الفرع 4", phone: "970598204148", avatar: "images/asmaa.png", status: "online" },
+    { id: 7, name: "عبدالرحمن", relation: "الفرع 5", phone: "970598204148", avatar: "images/abdulrahman.png", status: "busy" },
+    { id: 8, name: "عبدالله", relation: "الفرع 6", phone: "970598204148", avatar: "images/abdullah.png", status: "online" },
+    { id: 9, name: "مهيب", relation: "الفرع 7", phone: "970598204148", avatar: "images/mohaib.png", status: "offline" },
+    { id: 10, name: "يوسف", relation: "الفرع 8", phone: "970598204148", avatar: "images/youssef.png", status: "online" },
+    { id: 11, name: "أحمد", relation: "الفرع 9", phone: "970598204148", avatar: "images/ahmed.png", status: "online" }
+];
